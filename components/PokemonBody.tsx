6cb97320f88cd5e0a3f506b9e34323f28476f386
ModitTdr <modittuladhar@gmail.com>
2026-01-27 21:22:26 +0545

    feat: inifinte loading

--- components/PokemonBody.tsx
+++ components/PokemonBody.tsx
@@ -1,13 +1,57 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Input } from "./ui/input"
 import PokemonList from "./PokemonList";
 import { PokemonListProps } from "@/types/pokemon";
+import { getPokemonList } from "@/lib/PokemonApi";
 
 const PokemonBody = ({ pokemonList }: PokemonListProps) => {
+  const [pokemons, setPokemons] = useState(pokemonList);
   const [search, setSearch] = useState("");
-  const filteredList = pokemonList.filter((pokemon) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+
+  const LIMIT = 20;
+  const OFFSET = useRef(pokemons.length);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const loadMore = useCallback(async () => {
+    if (isLoading || !hasMore) return;
+
+    setIsLoading(true);
+    try {
+      const newPokemons = await getPokemonList(LIMIT, OFFSET.current);
+      OFFSET.current += LIMIT;
+
+      if (newPokemons.length === 0) {
+        setHasMore(false);
+      } else {
+        setPokemons((prev) => [...prev, ...newPokemons]);
+      }
+    } catch {
+      console.error("Error Loading Pokemon");
+    } finally {
+      setIsLoading(false);
+    }
+  }, [isLoading, hasMore]);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        loadMore();
+      }
+    }, {
+      threshold: 0.2
+    });
+
+    observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, [loadMore]);
+
+  const filteredList = pokemons.filter((pokemon) => {
     return pokemon.name.toLowerCase().includes(search.toLowerCase())
   })
   return (
@@ -19,7 +63,12 @@ const PokemonBody = ({ pokemonList }: PokemonListProps) => {
         onChange={(e) => setSearch(e.target.value)}
       />
       <PokemonList pokemonList={filteredList} />
-    </div>
+
+      <div ref={ref} className="h-10 flex items-center justify-center">
+        {isLoading && <p className="text-gray-500 animate-pulse">Loading more Pokemon...</p>}
+        {!hasMore && <p className="text-gray-500 animate-pulse">End of list...</p>}
+      </div>
+    </div >
   )
 }
 
--- lib/PokemonApi.ts
+++ lib/PokemonApi.ts
@@ -6,22 +6,13 @@ type PokemonListType = {
   name: string;
   url: string;
 };
-
-type PokemonDetailsType = {
-  name: string;
-  height: number;
-  weight: number;
-  abilities: { ability: { name: string } }[];
-  sprites: { front_default: string; other: { official_artwork: { front_default: string } } };
-  stats: { stat: { name: string }; base_stat: number }[];
-};
-
-export async function getPokemonList() {
-  const response = await axios.get(POKEMONAPI_URL + "pokemon?limit=10");
-  const pokemonDetailsPromises = response.data.results.map(async (pokemon: PokemonListType) => {
-    const pokemonDetails = await getPokemonData(pokemon.name);
-    return pokemonDetails;
-  });
+export async function getPokemonList(limit = 20, offset = 0) {
+  const response = await axios.get(`${POKEMONAPI_URL}pokemon?limit=${limit}&offset=${offset}`);
+  const pokemonDetailsPromises = response.data.results.map(
+    async (pokemon: PokemonListType) => {
+      const pokemonDetails = await getPokemonData(pokemon.name);
+      return pokemonDetails;
+    });
   const pokemonList = await Promise.all(pokemonDetailsPromises);
   return pokemonList;
 }
